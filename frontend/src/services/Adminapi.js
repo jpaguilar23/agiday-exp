51c0f87f7ff025ee7d4c9d8dca44f0499075b57d
jpaguilar23 <aguilarsaboriojp@gmail.com>
2026-08-08 12:32:59 -0600

    config: apuntar frontend a Render

--- frontend/src/services/Adminapi.js
+++ frontend/src/services/Adminapi.js
@@ -4,7 +4,9 @@
 
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://127.0.0.1:8000' })
+const api = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
+})
 
 // Interceptor para agregar token automáticamente
 api.interceptors.request.use(config => {
